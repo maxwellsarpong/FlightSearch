443c6997ffc4d88364abbaedb31078f2ec29db98
maxwellsarpong <maxwellsarpong07@gmail.com>
2025-04-04 17:46:24 +0000

    Initial Commit

---
+++ Flight/FlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightApp.swift
+//  Flight
+//
+//  Created by Maxwell Sarpong on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightTests/FlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTests.swift
+//  FlightTests
+//
+//  Created by Maxwell Sarpong on 04/04/2025.
+//
+
+import Testing
+@testable import Flight
+
+struct FlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
